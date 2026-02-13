e9fb69dc78f0f5e295092a2326d0ae2ab0975057
vishalyadavvv <vishalkumaryadav735@gmail.com>
2026-02-13 12:53:28 +0530

    clean

--- extract_models.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('models.json', 'utf8'));
-const providers = ['openai', 'google', 'perplexity'];
-const models = data.data.filter(m => providers.some(p => m.id.includes(p)));
-console.log(JSON.stringify(models.map(m => ({id: m.id, name: m.name})), null, 2));
